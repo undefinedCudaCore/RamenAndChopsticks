f05ed301c295421417fd7efa361878e4ad5ff948
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-04-17 20:29:53 +0300

    Add Order model props and constructor

--- RamenAndChopsticks/Models/Order.cs
+++ RamenAndChopsticks/Models/Order.cs
@@ -2,6 +2,28 @@
 {
     internal class Order
     {
-        public string OrderTableNumber { get; set; }
+
+        public Order(string orderTableId, int orderTableUsedSpaces, string orderDrinkId, double orderDrinkPriceWithVat,
+            string orderFoodId, double orderFoodPriceWithVat, double orderTotalPrice, DateTime orderStartDateTime)
+        {
+            OrderTableId = orderTableId;
+            OrderTableUsedSpaces = orderTableUsedSpaces;
+            OrderDrinkId = orderDrinkId;
+            OrderDrinkPriceWithVat = orderDrinkPriceWithVat;
+            OrderFoodId = orderFoodId;
+            OrderFoodPriceWithVat = orderFoodPriceWithVat;
+            OrderTotalPrice = orderTotalPrice;
+            OrderStartDateTime = orderStartDateTime;
+        }
+
+        public string OrderTableId { get; set; }
+        public int OrderTableUsedSpaces { get; set; }
+        public string OrderDrinkId { get; set; }
+        public double OrderDrinkPriceWithVat { get; set; }
+        public string OrderFoodId { get; set; }
+        public double OrderFoodPriceWithVat { get; set; }
+        public double OrderTotalPrice { get; set; }
+        public DateTime OrderStartDateTime { get; set; }
+        public DateTime OrderEndDateTime { get; set; }
     }
 }
