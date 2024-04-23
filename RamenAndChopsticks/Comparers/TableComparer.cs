f5fd44a857db54fcaf1eeda127672e4b76e5e70c
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-04-23 21:18:39 +0300

    Add table servicec tests

---
+++ RamenAndChopsticks/Comparers/TableComparer.cs
@@ -0,0 +1,12 @@
+﻿using RamenAndChopsticks.Models;
+
+namespace RamenAndChopsticks.Comparers
+{
+    public class TableComparer : IComparer<Table>
+    {
+        public int Compare(Table x, Table y)
+        {
+            return string.Compare(x.TableNumber, y.TableNumber);
+        }
+    }
+}
